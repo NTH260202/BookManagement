6a24649b86cee518716fe3c483453db793740210
NTH260202 <NTH260202@gmail.com>
2021-04-27 22:26:46 +0700

    create content Person

--- src/model/Person.java
+++ src/model/Person.java
@@ -1,5 +1,27 @@
 package model;
 
 public class Person {
-    
+    private String name;
+    private int id;
+
+    public Person(String name, int id) {
+        this.name = name;
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
 }
