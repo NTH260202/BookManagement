47ea127a65750855c874be6b9f9d1f60f9e7f6e4
NTH260202 <NTH260202@gmail.com>
2021-04-27 22:33:46 +0700

    create content in Person

--- src/model/Reader.java
+++ src/model/Reader.java
@@ -1,28 +1,5 @@
 package model;
 
-public class Reader extends Person {
-    private String address;
-    private int phone;
-
-    public Reader(String name, int id, String address, int phone) {
-        super(name, id);
-        this.phone = phone;
-        this.address = address;
-    }
-
-    public void setAddress(String address) {
-         this.address = address;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public void setPhone(int phone) {
-        this.phone = phone;
-   }
-
-   public int getPhone() {
-       return phone;
-   }
+public class Reader {
+    
 }
