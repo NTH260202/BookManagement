b1ff3e0682a0f943c5a2e0edc52d34e89f40d6bc
NTH260202 <NTH260202@gmail.com>
2021-04-28 16:25:59 +0700

    create content in BookDataController

--- src/controller/BookDataController.java
+++ src/controller/BookDataController.java
@@ -1,5 +1,44 @@
 package controller;
 
-public class BookDataController{
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Scanner;
 
+import model.Book;
+
+public class BookDataController{
+    private FileWriter fileWriter;
+    private BufferedWriter bufferedWriter;
+    private PrintWriter printWriter;
+    private Scanner scanner;
+
+    public void openFileToWrite() {
+        try {
+            fileWriter = new FileWriter("BOOK.DAT", true);
+            bufferedWriter = new BufferedWriter(fileWriter);
+            printWriter = new PrintWriter(bufferedWriter);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void writeBookToFile(Book book) {
+        openFileToWrite();
+        printWriter.println(book.getId() + "|" + book.getName() + "|" +
+                            book.getAuthor() + "|" + book.getSpecialization() + "|" + 
+                            book.getPublishYear() + "|" + book.getQuantity());
+        closeFileAfterWrite();
+    }
+
+    public void closeFileAfterWrite() {
+        try {
+            printWriter.close();
+            bufferedWriter.close();
+            fileWriter.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
