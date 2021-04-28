1078aa8b43c59820ca13c70508d00b7b038c17ed
NTH260202 <NTH260202@gmail.com>
2021-04-28 15:59:21 +0700

    create content in Book

--- src/model/Book.java
+++ src/model/Book.java
@@ -1,5 +1,49 @@
 package model;
 
-public class Book {
+public class Book extends Person{
+    private String author;
+    private int specialization;
+    private int publishYear;
+    private int quantity;
 
+    public Book(String name, int id, String author, int specialization, 
+                int publishYear, int quantity) {
+        super(name, id);
+        this.author = author;
+        this.specialization = specialization;
+        this.publishYear = publishYear;
+        this.quantity = quantity;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setSpecialization(int specialization) {
+        this.specialization = specialization;
+    }
+
+    public int getSpecialization() {
+        return specialization;
+    }
+
+    public void setPublishYear(int publishYear) {
+        this.publishYear = publishYear;
+    }
+
+    public int getPublishYear() {
+        return publishYear;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
 }
