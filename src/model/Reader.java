2270022278655abb79a75c164e6948313921f939
NTH260202 <NTH260202@gmail.com>
2021-04-30 15:52:38 +0700

    update readerId in Reader

--- src/model/Reader.java
+++ src/model/Reader.java
@@ -1,6 +1,8 @@
 package model;
 
 public class Reader extends Person{
+    private static int baseId = 10000000;
+    private int readerId;
     private String address;
     private int phone;
 
@@ -8,10 +10,39 @@ public class Reader extends Person{
         
     }
 
-    public Reader(String name, int id, String address, int phone) {
+    public Reader(int readerId) {
+        this.readerId = readerId;
+    }
+
+    public Reader(String name, int id) {
         super(name, id);
-        this.address = address;
+    }
+
+    public Reader(int readerId, String fullName, String address, int phone) {
+        super(fullName);
+        setReaderId();
         this.phone = phone;
+        this.address = address;
+    }
+
+    public static int getBaseId() {
+        return baseId;
+    }
+
+    public static void setBaseId(int baseId) {
+        Reader.baseId = baseId;
+    }
+
+    public void setReaderId() {
+        this.readerId = baseId++;
+    }
+
+    public void setReaderId(int id) {
+        readerId = id;
+    }
+
+    public int getReaderId() {
+        return readerId;
     }
 
     public void setAddress(String address) {
